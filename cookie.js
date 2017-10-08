8b2b011e706bcf2f0c4ab40bf4e91bdc0191a25e
sivanZhang <445080336@qq.com>
2017-10-08 12:19:16 +0800

    cookie function

---
+++ cookie.js
@@ -0,0 +1,46 @@
+var CookieUtil = {
+    get: function (name) {
+        var cookieName = encodeURIComponent(name) + "=",
+            cookieStart = document.cookie.indexOf(cookieName),
+            cookieValue = null;
+        if (cookieStart > -1) {
+            var cookieEnd = document.cookie.indexOf(";", cookieStart);
+            if (cookieEnd == -1) {
+                cookieEnd = document.cookie.length;
+            }
+            cookieValue = decodeURIComponent(document.cookie.substring(cookieStart
+                + cookieName.length, cookieEnd));
+        }
+        return cookieValue;
+    },
+    set: function (name,value,expires,path, domain, secure) {
+        var cookieText = encodeURIComponent(name) + "=" +
+            encodeURIComponent(value);
+        if (expires instanceof Date) {
+            cookieText += "; =" + expires.toGMTString();
+        }
+        if (path) {
+            cookieText += "; path=" + path;
+        }
+        if (domain) {
+            cookieText += "; domain=" + domain;
+        }
+        if (secure) {
+            cookieText += "; secure";
+        }
+        document.cookie = cookieText;
+    },
+    unset: function (name, path, domain, secure) {
+        this.set(name, "", new Date(0), path, domain, secure);
+    }
+};
+/* 
+设置cookie
+CookieUtil.set("name", "Nicholas");
+CookieUtil.set("book", "Professional JavaScript");
+读取cookie 的值
+alert(CookieUtil.get("name")); //"Nicholas"
+alert(CookieUtil.get("book")); //"Professional JavaScript"
+删除cookie
+CookieUtil.unset("name");
+CookieUtil.unset("book"); */
